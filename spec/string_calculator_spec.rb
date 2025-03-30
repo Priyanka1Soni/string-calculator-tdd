b0d2cc02db354a729e23e6b3f6d1d0b65ad1cec3
Priyanka soni <priyankasoni534@gmail.com>
2025-03-30 21:56:14 +0530

    Support Two Numbers (Comma-Separated)

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -11,5 +11,10 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1")).to eq(1)
       expect(StringCalculator.add("5")).to eq(5)
     end
+
+    it "returns sum of two comma-separated numbers" do
+      expect(StringCalculator.add("1,2")).to eq(3)
+      expect(StringCalculator.add("3,5")).to eq(8)
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,9 +1,14 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i
+    return numbers.to_i if !numbers.include?(",")
+
+    num_array = numbers.split(",").map(&:to_i)
+    num_array.sum
   end
 end
 
 # Example Usage
 puts StringCalculator.add("")       # Output: 0
+puts StringCalculator.add("1")      # Output: 1
+puts StringCalculator.add("1,5")    # Output: 6
